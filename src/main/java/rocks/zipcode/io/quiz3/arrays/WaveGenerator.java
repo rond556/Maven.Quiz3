effaad1ac33d3ec10220884290c2bf46659fa532
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-18 16:58:53 -0500

    final push

--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -1,10 +1,24 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class WaveGenerator {
     public static String[] wave(String str) {
-        return null;
+        String waveString = str.toLowerCase();
+        String[] waveArray = new String[str.length()];
+
+
+        for(int i = 0; i < str.length(); i++){
+            String waveSubString = "";
+            if(Character.isLetter(str.charAt(i))) {
+                waveSubString = waveString.substring(0, i) + waveString.substring(i, i + 1).toUpperCase() + waveString.substring(i + 1);
+            }
+            waveArray[i] = waveSubString;
+        }
+
+        return waveArray;
     }
 }
