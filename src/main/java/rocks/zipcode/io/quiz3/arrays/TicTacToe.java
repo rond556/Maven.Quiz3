c1dcb6541bc770c79888c3d220dc3ac0a69c1962
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-18 14:50:11 -0500

    tictactoe finished

--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -15,25 +15,54 @@ public class TicTacToe {
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        String[] values = new String[3];
+        for(int i = 0; i <= 2; i ++){
+            values[i] = board[value][i];
+        }
+        return values;
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+        String[] values = new String[3];
+        for(int i = 0; i <= 2; i ++){
+            values[i] = board[i][value];
+        }
+        return values;
     }
 
-    public Boolean isRowHomogenous(Integer rowIndex) {return null;
+    public Boolean isRowHomogenous(Integer rowIndex) {
+        if(board[rowIndex][0].equals(board[rowIndex][1]) && (board[rowIndex][1] == board[rowIndex][2])){
+            return true;
+        }
+        return false;
     }
 
     public Boolean isColumnHomogeneous(Integer columnIndex) {
-        return null;
+        if(board[0][columnIndex].equals(board[1][columnIndex]) && (board[1][columnIndex] == board[2][columnIndex])){
+            return true;
+        }
+        return false;
     }
 
     public String getWinner() {
-        return null;
+        for(int i = 0; i <= 2; i++){
+            if(isRowHomogenous(i)){
+                return board[i][i];
+            }
+        }
+        for(int i = 0; i <= 2; i++){
+            if(isColumnHomogeneous(i)){
+                return board[i][i];
+            }
+        }
+        if((board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2])) ||
+                (board[2][0].equals(board[1][1]) && board[1][1].equals(board[0][2]))){
+            return board[1][1];
+        }
+        return "";
     }
 
     public String[][] getBoard() {
-        return null;
+        return board;
     }
 }
