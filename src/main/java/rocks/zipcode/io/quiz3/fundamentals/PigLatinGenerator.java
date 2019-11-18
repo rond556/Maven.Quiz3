8f33fed8dcc11e1b3b120ed9efe826ede6333aca
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-18 17:33:07 -0500

    fixed up pig latin, now restructuring

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -5,43 +5,22 @@ package rocks.zipcode.io.quiz3.fundamentals;
  */
 public class PigLatinGenerator {
 
-    static Boolean isVowel(Character c){
-        return (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
-                c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-    }
-
     public String translate(String str) {
         StringBuilder sb = new StringBuilder();
         Integer length = str.length();
-        Integer index = -1;
+        Integer index = 1;
         String[] stringArray = str.split(" ");
 
         if(VowelUtils.startsWithVowel(str)) {return str + "way";}
         if (!VowelUtils.hasVowels(str)) {
             return str + "ay";
         }
-
-        for (int i = 0; i < length; i++) {
-            if (isVowel(str.charAt(i))) {
-                index = 1;
-                break;
-            }
-        }
-
         if(stringArray.length > 1) {
             for(String s : stringArray){
-                returnVowel(s);
                 sb.append(s.substring(index) + s.substring(0, index) + "ay ");
             }
             return sb.toString();
         }
         return str.substring(index) + str.substring(0, index) + "ay";
-    }
-
-    private boolean returnVowel(String str) {
-        if (isVowel(str.charAt(0))) {
-            return true;
-        }
-        return false;
     }
 }
